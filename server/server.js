a0c18821241c2c9a214aaab297d953b343feba82
w3techlabs-sys <w3techlabs@gmail.com>
2026-05-13 16:33:48 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,9 +7,20 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 const app = express();
+const router = express.Router();
 
 app.use(cors());
 app.use(express.json());
+
+import authRoutes from "./authRoutes.js";
+import serviceRoutes from "./serviceRoutes.js";
+import quoteRoutes from "./quoteRoutes.js";
+import contactRoutes from "./contactRoutes.js";
+
+router.use("/auth", authRoutes);
+router.use("/services", serviceRoutes);
+router.use("/quotes", quoteRoutes);
+router.use("/contact", contactRoutes);
 
 app.use("/api/admin", authRoutes);
 
